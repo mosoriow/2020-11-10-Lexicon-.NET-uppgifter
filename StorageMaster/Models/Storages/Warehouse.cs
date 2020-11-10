59c26a6402e5171696410f834b90b1900a97d254
Manuel Osorio Wallace <mosoriow@gmail.com>
2020-11-10 16:24:55 +0100

    2020-11-10 uppgifter

---
+++ StorageMaster/Models/Storages/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageMaster.Models.Storages
+{
+    class Warehouse : Storage
+    {
+        // creates a Warehouse with 3 semi trucks
+        public Warehouse(string name) : base(name, 10, 10, new[] { new Semi(), new Semi(), new Semi() } )
+        {
+
+        }
+    }
+}
